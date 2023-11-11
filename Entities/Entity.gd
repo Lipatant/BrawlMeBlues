e63344bfbcdebbc5319eb70b12b498cd3c8c0dd7
Lipatant <virgile.berrier@gmail.com>
2023-11-11 12:32:53 +0100

    feat: adding terrains and improving the movements

--- Entities/Entity.gd
+++ Entities/Entity.gd
@@ -4,8 +4,12 @@ class_name Entity
 # EXPORTS #
 
 @export var speed : float = 10000.0
-@export var jump_force : float = 60000.0
+@export var jump_force : float = 50000.0
 @export var jump_count : int = 2
+
+# ONREADIES #
+
+@onready var _wall_jump_movement_timer : Timer = $WallJumpMovement
 
 # OTHER VARIABLES #
 
@@ -13,43 +17,76 @@ var _motions : Array[Dictionary] = []
 var _jumps : int = 0
 var _since_floor : float = 0.0
 
+var _wall_jump_movement : float = 0.0
+
 # CONSTS #
 
 const BASE_GRAVITY : float = 16000.0
+
+const jump_particles_resource : Resource = preload("res://Particles/EntityJumpParticles.tscn")
+const ceiling_particles_resource : Resource = preload("res://Particles/EntityCeilingParticles.tscn")
 
 # PROCESS #
 
 func _physics_process(delta: float) -> void:
 	velocity.x = 0
+	# Gravity
 	if _since_floor >= 1.0:
-		velocity.y = _get_gravity()
+		velocity.y = _get_gravity_force()
 	elif _since_floor > 0.0:
-		velocity.y = _get_gravity() * (0.5 + _since_floor / 2)
-	if is_on_wall():
+		velocity.y = _get_gravity_force() * (0.5 + _since_floor / 2)
+	if is_wall_sliding():
 		_remove_motion_from_id("jump")
 		velocity.y /= 5
-	velocity += _entity_movement() * speed
+	# Entity Movement
+	var entity_moment : Vector2 = _entity_movement()
+	if !_wall_jump_movement_timer.is_stopped():
+		entity_moment.x *= 1 - (_wall_jump_movement_timer.time_left / _wall_jump_movement_timer.wait_time)
+		entity_moment.x += _wall_jump_movement * (0.75 + (_wall_jump_movement_timer.time_left / _wall_jump_movement_timer.wait_time) / 4)
+	if entity_moment.x > 1.0:
+		entity_moment.x = 1.0
+	elif entity_moment.x < -1.0:
+		entity_moment.x = -1.0
+	velocity += entity_moment * speed
+	#
 	velocity += _process_motion(delta)
 	velocity *= delta
 	move_and_slide()
 	if is_on_ceiling():
-		_remove_motion_from_id("jump")
+		if _remove_motion_from_id("jump"):
+			generate_particles(ceiling_particles_resource)
 	if is_on_floor():
 		_since_floor = 0
 		_jumps = jump_count
 	else:
 		_since_floor += delta
-	if is_on_wall():
-		_jumps = 1 if jump_count >= 1 else jump_count
+	if is_wall_sliding():
+#		_jumps = 1 if jump_count >= 1 else jump_count
+		_jumps = jump_count
 		if _since_floor > 0.2:
 			_since_floor = 0.2
 
 func _entity_movement() -> Vector2:
 	return Vector2(0, 0)
 
+func is_wall_sliding(only: bool = false) -> bool:
+	return is_on_wall_only() if only else is_on_wall()
+
+# JUMP #
+
+func _entity_jump(force: bool = false) -> void:
+	if force or _jumps > 0:
+		if !force:
+			_jumps -= 1
+		add_motion({"motion": Vector2(0, -jump_force), "duration": 0.5, "decreasing": true, "id": "jump"})
+		generate_particles(jump_particles_resource)
+		if is_wall_sliding(true):
+			_wall_jump_movement = 1.2 if get_wall_normal().x > 0 else -1.2
+			_wall_jump_movement_timer.start()
+
 # GET CONSTANTS #
 
-static func _get_gravity() -> float:
+static func _get_gravity_force() -> float:
 	return BASE_GRAVITY
 
 # MOTIONS #
@@ -114,3 +151,14 @@ func _remove_motion_from_id(id: String = "none", force: bool = false) -> bool:
 		else:
 			motion_id += 1
 	return true
+
+# PARTICLES
+
+func generate_particles(resource: Resource) -> Node2D:
+	if !resource:
+		return null
+	var particles : Node2D = resource.instantiate()
+	if particles:
+		get_tree().current_scene.add_child(particles)
+		particles.global_position = global_position
+	return particles
